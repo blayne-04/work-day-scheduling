2468786e417e6f5d72db84fde0e940a8d629b58d
blayne-04 <befuller2004@gmail.com>
2023-01-30 06:40:44 -0800

    reorganized index.html+cut emptyfunc in script.js

--- assets/script.js
+++ assets/script.js
@@ -27,12 +27,11 @@ for (var i = 0; i < hours.length; i++) {
   $('#hr-' + hour).children('textarea').val(item);
 }
 
-$(function () {
-  console.log()
-    // TODO: Add code to apply the past, present, or future class to each time
+
+hours 
+ // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-  });
 })
